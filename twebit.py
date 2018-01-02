f59582c323e58d209d8f0a7524368de866185186
Ömer Günal <o.gunal977@gmail.com>
2018-01-02 12:57:56 +0300

    Update twebit.py

--- twebit.py
+++ twebit.py
@@ -13,24 +13,9 @@ consumerSecret="XXXX"
 accessToken="XXXX-XXXX"
 accessSecret="XXXX"
 
-train = []
-for i in open('data/pos.txt'): # get train data from pos.txt and neg.txt
-        train.append([i.rstrip('\n'), 'pos'])
-for j in open('data/neg.txt'):
-        train.append([j.rstrip('\n'), 'neg'])
-
-test = []
-for z in open('data/test/testPos.txt'): # get test data from testPos.txt and testNeg.txt
-    test.append([z.rstrip('\n'), 'pos'])
-for x in open('data/test/testNeg.txt'):
-    test.append([x.rstrip('\n'), 'neg'])
-
-cl = NaiveBayesClassifier(train)
 
 class listener(StreamListener):
-
     def on_data(self, data):
-
         all_data = json.loads(data)
         tweet = all_data["text"]
         analysis = TextBlob(tweet)
@@ -42,17 +27,32 @@ class listener(StreamListener):
     def on_error(self, status):
         print (status)
 
-auth = OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessSecret)
 
-while True:
-    twitterStream = Stream(auth, listener())
-    twitterStream.filter(track=["bitcoin"], async = True)
-    time.sleep(60)
-    twitterStream.disconnect()
 
-'''# Classify some text
-print(cl.classify("bitcoin is rising to $20000"))  # "pos"
-print(cl.classify("Bitcoin is a big bubble"))   # "neg"
+def main():
+    train = []
+    for i in open('data/pos.txt'): # get train data from pos.txt and neg.txt
+            train.append([i.rstrip('\n'), 'pos'])
+    for j in open('data/neg.txt'):
+            train.append([j.rstrip('\n'), 'neg'])
 
-print(cl.accuracy(test))'''
+    test = []
+    for z in open('data/test/testPos.txt'): # get test data from testPos.txt and testNeg.txt
+        test.append([z.rstrip('\n'), 'pos'])
+    for x in open('data/test/testNeg.txt'):
+        test.append([x.rstrip('\n'), 'neg'])
+
+    global cl
+    cl = NaiveBayesClassifier(train)
+
+    auth = OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessToken, accessSecret)
+
+    while True:
+        twitterStream = Stream(auth, listener())
+        twitterStream.filter(track=["bitcoin"], async = True)
+        time.sleep(60)
+        twitterStream.disconnect()
+
+if __name__ == '__main__':
+    main()
